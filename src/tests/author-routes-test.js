13e134f6efecb1f21e50ab75b223e3c1f5c13ad8
MrAzuka <johntwice8@gmail.com>
2022-06-27 14:11:40 +0100

    Home test done

--- src/tests/author-routes-test.js
+++ src/tests/author-routes-test.js
@@ -1,7 +1,9 @@
 const chai = require('chai')
 const chaiHttp = require('chai-http')
+const { describe } = require('mocha')
 
-const server = require('../server')
+const server = require('../app')
+const User = require('../models/author-model')
 
 const expect = chai.expect
 
@@ -12,3 +14,23 @@ const testAuthor = {
     email: "johntwice8@gmail.com",
     password: "testpassword1234"
 }
+
+describe("Test Author Authentication Route", () => {
+    before(async () => {
+        //delete al occurences of the test user from the db
+        await User.deleteMany({ email: testAuthor.email });
+    })
+    describe("Test Registration Route", () => {
+        it("Register Author", (done) => {
+            chai.request(server)
+            .post("/register")
+            .send(testAuthor)
+            .end((err,res) => {
+                expect(err).to.be.null
+    
+            })
+        })
+    })
+    
+    
+})
